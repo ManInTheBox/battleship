1ce60eb5cb8299b819223d2d7a4d46bc5a0b0742
Zarko Stankovic <stankovic.zarko@gmail.com>
2020-04-05 16:39:21 +0200

    Add method to determine the ship type.

--- lib/battleship/grid.ex
+++ lib/battleship/grid.ex
@@ -1,34 +1,26 @@
 defmodule Battleship.Grid do
   @size 1..10
+  @ship_count %{submarine: 4, destroyer: 3, cruiser: 2, battleship: 1}
 
   def dimensions, do: @size
+  def new, do: []
 
-  def new() do
-    %{
-      allowed_ships: %{submarine: 4, destroyer: 3, cruiser: 2, battleship: 1},
-      occupied: []
-    }
+  def add_ship(grid, ship) do
+    ship_type(ship)
   end
 
-  def add_ship(grid, {square1}) do
-    with {:ok, allowed_ships} <- decrement_allowed_ships(grid.allowed_ships, :submarine),
-         {:ok, occupied} <- occupy_squares(grid.occupied, square1) do
-      %{allowed_ships: allowed_ships, occupied: occupied}
+  defp ship_type(ship) when is_tuple(ship) do
+    case tuple_size(ship) do
+      1 ->
+        :submarine
+      2 ->
+        :destroyer
+      3 ->
+        :cruiser
+      4 ->
+        :battleship
+      _ ->
+        {:error, :unknown_ship_type}
     end
-  end
-
-  defp decrement_allowed_ships(allowed_ships, type) do
-    if allowed_ships[type] === 0 do
-      {:error, :all_ships_arranged, type}
-    else
-      allowed_ships = Map.update!(allowed_ships, type, fn current -> current - 1 end)
-      {:ok, allowed_ships}
-    end
-  end
-
-  defp occupy_squares(occupied, square1) do
-    occupied = [square1 | occupied]
-    occupied = Enum.sort(occupied)
-    {:ok, occupied}
   end
 end
