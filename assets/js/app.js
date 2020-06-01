1ae3702d243d952a26bf826709737f27ff288734
Zarko Stankovic <stankovic.zarko@gmail.com>
2020-06-01 17:42:22 +0200

    Remove unused code

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,7 @@ import css from "../css/app.css"
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+let liveSocket = new LiveSocket("/live", Socket);
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
